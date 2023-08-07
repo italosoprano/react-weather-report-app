15afb931961219c7d03259c79587afb7a053682f
Ítalo Soprano <110266373+italosoprano@users.noreply.github.com>
2023-08-07 17:52:20 -0400

    update!

---
+++ weather-report-app/src/componentes/pesquisa.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Pesquisa from '../css/pesquisa.css'
+
+
+export default function pesquisa() {
+
+    return(
+        <div className=''>
+        
+        </div>
+    )
+}
