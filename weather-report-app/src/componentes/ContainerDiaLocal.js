843f4ab0f117efd5a70b67f05e153968c6a0f91a
Ítalo Soprano <110266373+italosoprano@users.noreply.github.com>
2023-08-20 19:00:50 -0400

    update

--- weather-report-app/src/componentes/ContainerDiaLocal.js
+++ weather-report-app/src/componentes/ContainerDiaLocal.js
@@ -1,10 +1,9 @@
-import React from "react";
-import '../css/ContainerDiaLocal.css'
+import React from 'react';
+import '../css/ContainerDiaLocal.css';
 
-export default function ContainerDiaLocal(props){
-
+export default function ContainerDiaLocal(props) {
     function diaSemana() {
-        const dia = new Date()
+        const dia = new Date();
         const diaSem = {
             0: 'Domingo',
             1: 'Segunda-feira',
@@ -12,16 +11,18 @@ export default function ContainerDiaLocal(props){
             3: 'Quarta-feira',
             4: 'Quinta-feira',
             5: 'Sexta-feira',
-            6: 'Sabádo'
-        }
+            6: 'Sabádo',
+        };
 
-        return diaSem[dia.getDay()]
+        return diaSem[dia.getDay()];
     }
 
     return (
         <div className="ContainerDiaLocal">
             <h2>{props.Cidade}</h2>
-            <p>{diaSemana()}, {new Date().getDate()}</p>
+            <p>
+                {diaSemana()}, {new Date().getDate()}
+            </p>
         </div>
-    )
+    );
 }
--- weather-report-app/src/componentes/template.js
+++ weather-report-app/src/componentes/template.js
@@ -1,55 +1,61 @@
-import React, {useState, useEffect} from 'react'
-import '../css/template.css'
-import DiaSemana from './ExpoDiaSem'
-import Sol from '../componentes/sol.png'
-import ContainerDiaLocal from '../componentes/ContainerDiaLocal'
-import ContainerImg from '../componentes/ContainerImg'
-import ContainerTemp from '../componentes/ContainerTemp'
-import ContainerInfo from '../componentes/ContainerInfo'
-
+import React, { useState, useEffect } from 'react';
+import '../css/template.css';
+import DiaSemana from './ExpoDiaSem';
+import Sol from '../componentes/sol.png';
+import ContainerDiaLocal from '../componentes/ContainerDiaLocal';
+import ContainerImg from '../componentes/ContainerImg';
+import ContainerTemp from '../componentes/ContainerTemp';
+import ContainerInfo from '../componentes/ContainerInfo';
 
 export default function Template() {
+  const [cidade, setCidade] = useState('');
+  const [weatherForecast, setWeatherForecast] = useState(null);
 
-    const [cidade, setCidade]=useState('')
-    const [weatherForecast, setWeatherForecast]=useState(null)
+  const handleChange = (e) => {
+    setCidade(e.target.value);
+  };
 
-    const handleChange = (e) =>{
-        setCidade(e.target.value)
-    }
-
-    const handleSearch = () =>{
-        fetch(`http://api.weatherapi.com/v1/forecast.json?key=1cc0abe572ee47eca0a04826231008&q=${cidade}&lang=pt`)
-        .then((res)=>{
-            if(res.status == 200){
-                return res.json()
-            }
-        })
-        .then((data)=>{
-            setWeatherForecast(data)
-            console.log(weatherForecast)
-        })
-    }
-
-    return(
-        <div className='Main'>
-            <div className='CaixaDePesquisa'>
-                <form>
-                    <input 
-                    type='text' 
-                    className='InputNameCity' 
-                    placeholder='Digite uma cidade' 
-                    value={cidade}
-                    onChange={handleChange}
-                    />
-                </form>
-                <button onClick={handleSearch}>Pesquisar</button>
-            </div>
-            
-            <div className='VitrineDoTempo'>
-                <ContainerDiaLocal Cidade={weatherForecast?weatherForecast.location.name:"Manaus"}/>
-                <ContainerImg Src={weatherForecast?weatherForecast.current.condition.icon: Sol}/>
-                <ContainerTemp Temp_c={weatherForecast?weatherForecast.current.temp_c: '37'} Text={weatherForecast?weatherForecast.current.condition.text: 'Céu nublado, com chuva.'} Sensa={weatherForecast?weatherForecast.current.feelslike_c + "ºC": '9 bilhões de ºC'}/>
-            </div>
-        </div>
+  const handleSearch = () => {
+    fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=1cc0abe572ee47eca0a04826231008&q=${cidade}&lang=pt`
     )
+      .then((res) => {
+        if (res.status == 200) {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        setWeatherForecast(data);
+        console.log(weatherForecast);
+      });
+  };
+
+  return (
+    <div className="Main">
+      <div className="CaixaDePesquisa">
+        <form>
+          <input
+            type="text"
+            className="InputNameCity"
+            placeholder="Digite uma cidade"
+            value={cidade}
+            onChange={handleChange}
+          />
+        </form>
+        <button onClick={handleSearch}>Pesquisar</button>
+      </div>
+
+      <div className="VitrineDoTempo">
+        <ContainerDiaLocal Cidade={weatherForecast ? weatherForecast.location.name : 'Manaus'} />
+        <ContainerImg Src={weatherForecast ? weatherForecast.current.condition.icon : Sol} />
+        <ContainerTemp
+          Temp_c={weatherForecast ? weatherForecast.current.temp_c : '37'}
+          Text={
+            weatherForecast ? weatherForecast.current.condition.text : 'Céu nublado, com chuva.'
+          }
+          Sensa={weatherForecast ? weatherForecast.current.feelslike_c + 'ºC' : '9 bilhões de ºC'}
+        />
+      </div>
+    </div>
+  );
 }
