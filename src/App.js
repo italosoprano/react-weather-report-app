ca802bbb38979964a5c7f670fe1ff56168b85084
Ítalo Soprano <110266373+italosoprano@users.noreply.github.com>
2023-10-27 22:55:42 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Main from './componentes/Main';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
     return (
-        <div className="App">
+        <div className="App vh-100">
             <Main></Main>
         </div>
     );
